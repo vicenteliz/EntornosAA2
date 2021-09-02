bf18dfdc715124a4feed8c66e683690c16741f17
VicenteLizandra <vicentury@gmail.com>
2021-09-02 23:50:41 +0200

    Add application

---
+++ src/main/java/com/mycompany/entornosaa2/Application.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.entornosaa2;
+
+/**
+ *
+ * @author Vicente
+ */
+public class Application {
+
+    
+    public static void main(String[] args) {
+        
+        Agencia agencia = new Agencia();
+        
+        agencia.ejecutar();
+        
+    }
+    
+    
+}
